5dde5c918290fc1c34d865ac27f45bed04bc6498
aswin <aswin@DESKTOP-TK3J6EM>
2020-04-16 13:24:47 +0530

    Updated for smooth flow

--- nykaa/src/main/java/project/nykaa/nykaa.java
+++ nykaa/src/main/java/project/nykaa/nykaa.java
@@ -26,6 +26,7 @@ public class nykaa {
 		driver.get("https://www.nykaa.com/");
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		
+		
 		//Mouse over on brand
 		Actions action = new Actions(driver);
 		action.moveToElement(driver.findElement(By.xpath("//a[text()='brands']"))).build().perform();
@@ -34,7 +35,10 @@ public class nykaa {
 		Thread.sleep(3000);
 		action.moveToElement(driver.findElement(By.xpath("//li[@class='brand-logo menu-links'][5]//img"))).click().build().perform();
 		
+		
+		//Used to switch to the PARENT WINDOW from any page
 		String parentWindow = driver.getWindowHandle();
+		
 		
 		//Switch to the page "L'Oreal Paris"
 		Set<String> windows = driver.getWindowHandles();
@@ -42,12 +46,14 @@ public class nykaa {
 		noOfWindows.addAll(windows);
 		driver.switchTo().window(noOfWindows.get(1));
 		
+		
 		//To verify the title of the page
 		if (driver.getTitle().contains("L'Oreal Paris")) {
 			System.out.println("Title is matching with the page");
 		}else {
 			System.out.println("Title doesnt match with the page");
 		}
+		
 		
 		//Scroll page to view correct option is selected
 		JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -63,11 +69,10 @@ public class nykaa {
 	
 		wait = new WebDriverWait(driver, 30);
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[text()='Category']"))).click();
-		
 		Thread.sleep(3000);
-		
 		wait = new WebDriverWait(driver,30);
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='chk_Shampoo_undefined']/span"))).click();
+		
 		
 		//To verify the "SHAMPOO" is filtered
 		String selectedText = driver.findElement(By.xpath("//li[text()='Shampoo']")).getText();
@@ -78,10 +83,10 @@ public class nykaa {
 			System.out.println("Filter is not applied");
 		}
 		
+		
 		//Switching to the window2(L'Oreal Paris Colour Protect Shampoo) page
 		wait = new WebDriverWait(driver,30);
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()=\"L'Oreal Paris Colour Protect Shampoo\"]"))).click();
-		//driver.findElement(By.xpath("//span[text()=\"L'Oreal Paris Colour Protect Shampoo\"]")).click();
 		
 		Set<String> windows2 = driver.getWindowHandles();
 		List<String> noOfWindows2 = new ArrayList<String>(windows2);
@@ -96,12 +101,13 @@ public class nykaa {
 		String priceOfShampoo = driver.findElement(By.xpath("//div[@class='clearfix product-des__details']//span[2]/span")).getText();
 		System.out.println("SHAMPOO PRICE IS:"+priceOfShampoo);
 		
+		
 		//Select the quantity(ML) of the shampoo and add it to the BAG
 		wait = new WebDriverWait(driver, 30);
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='pull-left']/div/button"))).click();
-		
 		Thread.sleep(3000);
 		driver.findElement(By.className("AddBagIcon")).click();
+		
 		
 		//Print the grand total amount
 		String grandTotal = driver.findElement(By.xpath("//div[@class='payment-tbl-data']/div[4]/div[2]")).getText();
@@ -127,11 +133,5 @@ public class nykaa {
 		driver.close();		
 		
 	}
-
-	private static Function<? super WebDriver, Object> ExpectedConditions(By xpath) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
 
 }
